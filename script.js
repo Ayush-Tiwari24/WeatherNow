38ced05c54ec79456f369145c13f408abaa41fdc
Ayush-Tiwari24 <ranjanayush2021@gmail.com>
2026-07-02 23:10:26 +0530

    major changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const search = document.querySelector(".Search");
 const apikey = "7c49ac95fc82605c796707d9c2a1af41";
-
+let clockint;
 let city;
 
 function getWindDirection(degree) {
@@ -47,10 +47,11 @@ function weather(url, apikey) {
             alert("City not found")
             return;
         }
+        console.log(data)
         const desc = data.weather[0].description;
         document.querySelector(".weather-disc").textContent = desc.charAt(0).toUpperCase() + desc.slice(1);
         document.querySelector(".cel").textContent = `${Math.round(data.main.temp)}°`;
-        document.querySelector(".city-name").innerHTML = `<i class="fa-solid fa-location-dot" style="color:rgb(62, 145, 177)"></i> ` + data.name;
+        document.querySelector(".city-name").innerHTML = `<i class="fa-solid fa-location-dot" style="color:rgb(62, 145, 177)"></i> ` + data.name+","+data.sys.country;
         document.querySelector(".hi").textContent = `${Math.round(data.main.temp_max)}°`;
         document.querySelector(".low").textContent = `${Math.round(data.main.temp_min)}°`;
         document.querySelector(".humidity").textContent = `${data.main.humidity}%`;
@@ -62,23 +63,22 @@ function weather(url, apikey) {
 
         fetch(aqiurl).then((response) => response.json()).then((aqiData) => {
             const aqi = aqiData.data.aqi;
-            console.log(aqiData)
-            console.log(aqi)
+        
             document.querySelector(".aqi").textContent = aqi;
         });
+
         fetch(aqiqurl).then((response) => response.json()).then((aqiqData) => {
             const aqi = aqiqData.list[0].main.aqi;
-            console.log(aqiqData)
-            const qualities = ["", "Good", "Fair", "Moderate", "Poor", "Very Poor"];
+            const qualities = ["", "Good", "Moderate", "Poor","Unhealthy", "Hazardous"];
             document.querySelector(".quality").textContent = qualities[aqi];
         });
+
         document.querySelector(".wind").textContent = `${data.wind.speed}m/s`;
         document.querySelector(".dir").textContent = getWindDirection(data.wind.deg);
         document.querySelector(".like").textContent = `${Math.round(data.main.feels_like)}°`;
         document.querySelector(".feels").textContent = getFeelsLikeStatus(Math.round(data.main.feels_like))
         document.querySelector(".visibility").textContent = `${(data.visibility / 1000).toFixed(1)} km`;
         document.querySelector(".visible").textContent = getVisibilityStatus(data.visibility);
-        console.log(data)
 
         //5 days forecast
         const forecasturl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
@@ -99,12 +99,49 @@ function weather(url, apikey) {
                                   <p class="weektemp">${temp}°C</p>`;
             });
         })
+
+        const timezone = data.timezone;
+                const utc = new Date().getTime() + new Date().getTimezoneOffset() * 60000;
+
+                let cityTime = new Date(utc + timezone * 1000);
+
+                const hour = cityTime.getHours();
+                if (hour >= 5 && hour < 11) {
+
+                    document.body.style.backgroundImage =
+                        "url('sunrise.jpeg')";
+
+                }
+                else if (hour >= 11 && hour < 17) {
+
+                    document.body.style.backgroundImage =
+                        "url('wallpaper-edited.jpeg')";
+
+                }
+                else if (hour >= 17 && hour < 20) {
+
+                    document.body.style.backgroundImage =
+                        "url('sunset.jpeg')";
+
+                }
+                else {
+
+                    document.body.style.backgroundImage =
+                        "url('https://wallpaperaccess.com/full/941788.jpg')";
+
+                }
+                // let clockint;
+                clearInterval(clockint);
+                clockint=setInterval(()=>{
+                    const utc = Date.now() + new Date().getTimezoneOffset() * 60000;
+                    cityTime = new Date(utc + timezone * 1000);
+                    document.querySelector(".timecon").innerHTML=`${cityTime.toLocaleTimeString()}`;
+                },1000);
     });
 
 }
-search.addEventListener("keydown", function(e) {
+search.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        console.log(search.value);
         city = search.value;
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
         weather(url, apikey);
@@ -122,8 +159,9 @@ window.addEventListener("load", () => {
 
         },
         (err) => {
-            console.log(err);
+            
             alert("Location access denied");
         }
     )
 })
+
